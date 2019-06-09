df3fbc0aa86de31747e2813f31e1fc8a02df240a
b54690 <belafonte@hotmail.co.uk>
2019-06-09 13:20:11 +0200

    resolved issues with updates to angular devkit using ng update @..

--- CatApiMeow/src/app/modules/search.module.ts
+++ CatApiMeow/src/app/modules/search.module.ts
@@ -2,12 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SearchComponent} from '../components/search.component';
 import {SearchService} from '../infrastrcuture/services/search.service';
-import {MatSelect} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule,
-    MatSelect
+    CommonModule
   ],
   declarations: [
     SearchComponent
