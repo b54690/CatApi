a4054656d218477f1e05c4830cc077b25925887d
b54690 <belafonte@hotmail.co.uk>
2019-06-09 13:28:36 +0200

    html added to show dog breed from API in dropdown working succeesfully

--- CatApiMeow/src/app/modules/search.module.ts
+++ CatApiMeow/src/app/modules/search.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SearchComponent} from '../components/search.component';
 import {SearchService} from '../infrastrcuture/services/search.service';
+import {MatFormFieldModule, MatOptionModule, MatSelectModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   declarations: [
     SearchComponent
